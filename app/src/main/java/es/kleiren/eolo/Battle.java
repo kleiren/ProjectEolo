a543604dea12e9f6ee30876943a49368f9971b9a
kleiren <csanchezredondo@gmail.com>
2016-10-16 22:13:49 +0200

    -First "more or less complete" app. -New assets. -Simple battle implemented -All map stuff fixed and working :)

--- app/src/main/java/es/kleiren/eolo/Battle.java
+++ app/src/main/java/es/kleiren/eolo/Battle.java
@@ -1,5 +1,6 @@
 package es.kleiren.eolo;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,40 +8,88 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Battle extends AppCompatActivity {
 
-    ImageView imagePlayer;
-    Animation moveInLeft, moveInRight, attack;
+    ImageView imagePlayer, enemyHp;
+    Animation moveInLeft, moveInRight, attack, shrink;
     RelativeLayout layoutPlayer, layoutEnemy;
-    View battleView;
+    TextView txtInfo, txtEnemy, txtPlayer;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle);
 
-       // overridePendingTransition(R.anim.move_in_left, R.anim.move_in_left);
+        context = this;
+
+        txtInfo = (TextView) findViewById(R.id.infotext);
+        txtEnemy = (TextView) findViewById(R.id.enemyname);
+        txtPlayer = (TextView) findViewById(R.id.playername);
+
+        txtInfo.setText("A wild Asrobot appeared!");
+        txtPlayer.setText("Rednamrahc");
+        txtEnemy.setText("Asrobot");
 
         imagePlayer = (ImageView) findViewById(R.id.imagePlayer);
-
+        enemyHp = (ImageView) findViewById(R.id.enemyhp);
         layoutEnemy = (RelativeLayout) findViewById(R.id.layout_enemy);
         layoutPlayer = (RelativeLayout) findViewById(R.id.layout_player);
-
 
         moveInLeft = AnimationUtils.loadAnimation(this, R.anim.move_in_left);
         moveInRight = AnimationUtils.loadAnimation(this, R.anim.move_in_right);
         attack = AnimationUtils.loadAnimation(this, R.anim.attack);
-
+        shrink = AnimationUtils.loadAnimation(this, R.anim.shrink);
 
         layoutPlayer.setAnimation(moveInLeft);
         layoutEnemy.setAnimation(moveInRight);
 
+        attack.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                txtInfo.setText("It was very effective!");
+                enemyHp.startAnimation(shrink);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        shrink.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
     }
 
     public void attack(View v){
-        Toast.makeText(this, "attack", Toast.LENGTH_SHORT).show();
+        txtInfo.setText("Attack Rednamrahc!");
         imagePlayer.startAnimation(attack);
     }
+
+
+
+
 }
--- app/src/main/java/es/kleiren/eolo/MainActivity.java
+++ app/src/main/java/es/kleiren/eolo/MainActivity.java
@@ -1,34 +1,27 @@
 package es.kleiren.eolo;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ScaleDrawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.Toast;
 
 import org.osmdroid.api.IMapController;
-import org.osmdroid.tileprovider.tilesource.ITileSource;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.tileprovider.tilesource.XYTileSource;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
-import org.osmdroid.views.overlay.ItemizedOverlay;
 import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
 import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.compass.CompassOverlay;
@@ -37,21 +30,23 @@ import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     private MyLocationNewOverlay mLocationOverlay;
-
     private Context context;
-    private GeoPoint currentLocation;
     private LocationManager locationManager;
     private CompassOverlay mCompassOverlay;
-
+    private MapView map;
+    private ItemizedOverlayWithFocus<OverlayItem> mOverlay;
     private Button btn_forceBattle;
+    private IMapController mapController;
+    private GeoPoint monsterLocation;
+    private GeoPoint currentLocation;
 
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,8 +58,7 @@ public class MainActivity extends AppCompatActivity {
         btn_forceBattle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Battle.class);
-                startActivity(intent);
+                startBattle();
             }
         });
 
@@ -72,15 +66,14 @@ public class MainActivity extends AppCompatActivity {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 100,
+                this);
 
-        //important! set your user agent to prevent getting banned from the osm servers
+        //Important! set your user agent to prevent getting banned from the osm servers
         org.osmdroid.tileprovider.constants.OpenStreetMapTileProviderConstants.setUserAgentValue(BuildConfig.APPLICATION_ID);
 
-        MapView map = (MapView) findViewById(R.id.map);
-//        map.setTileSource(TileSourceFactory.MAPNIK);
-
-       // GpsMyLocationProvider me = new GpsMyLocationProvider(this);
-
+        // Map definition
+        map = (MapView) findViewById(R.id.map);
 
         this.mLocationOverlay = new MyLocationNewOverlay( new GpsMyLocationProvider(context),map);
         map.getOverlays().add(this.mLocationOverlay);
@@ -88,48 +81,39 @@ public class MainActivity extends AppCompatActivity {
         this.mCompassOverlay = new CompassOverlay(context, new InternalCompassOrientationProvider(context), map);
         map.getOverlays().add(this.mCompassOverlay);
 
-
-
-
-
-
         // lon = -3.7678304314613342
         // lat = 40.33183226746575
 
 
-     //   Drawable marker = getDrawable(R.drawable.center);
-      //  ItemizedIconOverlay myItemizedOverlay = new ItemizedIconOverlay();
-
-
-       // myItemizedOverlay.addItem(myPoint1, "myPoint1", "myPoint1");
-
-        final IMapController mapController = map.getController();
+        mapController = map.getController();
         mapController.setZoom(19);
         map.setTilesScaledToDpi(true);
-
         mLocationOverlay.enableMyLocation();
-
+        mapController.stopPanning();
         mCompassOverlay.enableCompass();
         mapController.animateTo(new GeoPoint(40.33183226746575, -3.7678304314613342));
+    }
 
-        mLocationOverlay.enableFollowLocation();
-        mLocationOverlay.runOnFirstFix(new Runnable(){
-            public void run() {
-                mapController.animateTo(mLocationOverlay
-                        .getMyLocation());
-            }
-        });
-
-/*
+    public void addMonsters(GeoPoint currentLocation){
         //your items
         ArrayList<OverlayItem> items = new ArrayList<OverlayItem>();
+        Drawable newMarker = this.getResources().getDrawable(R.drawable.ic_asrobot_pix);
+        monsterLocation = getLocation(currentLocation, 100);
+        OverlayItem monster = new OverlayItem("ASROBOT", "Hold to fight", monsterLocation);
+        monster.setMarker(newMarker);
 
-//the overlay
-        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(items,
+        items.add(monster);
+
+        //the overlay
+        mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(items,
                 new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
                     @Override
                     public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
-                        //do something
+                        
+                        if (mLocationOverlay.getMyLocation().distanceTo(monsterLocation) < 50)
+                        startBattle();
+                        else
+                            Toast.makeText(context, "You are too far away! Get near", Toast.LENGTH_SHORT).show();
                         return true;
                     }
                     @Override
@@ -137,8 +121,73 @@ public class MainActivity extends AppCompatActivity {
                         return false;
                     }
                 }, context);
-        mOverlay.setFocusItemsOnTap(true);
 
-        map.getOverlays().add(mOverlay);*/
+
+        map.getOverlays().add(mOverlay);
+
+
+    }
+
+    public void startBattle(){
+        mOverlay.removeAllItems();
+        Intent intent = new Intent(getApplicationContext(), Battle.class);
+        startActivityForResult(intent, 1);
+    }
+
+
+    public static GeoPoint getLocation(GeoPoint location,  int radius) {
+        GeoPoint result = new GeoPoint(location);
+        Random random = new Random();
+
+        // Convert radius from meters to degrees
+        double radiusInDegrees = radius / 111000f;
+
+        double u = random.nextDouble();
+        double v = random.nextDouble();
+        double w = radiusInDegrees * Math.sqrt(u);
+        double t = 2 * Math.PI * v;
+        double x = w * Math.cos(t);
+        double y = w * Math.sin(t);
+
+        // Adjust the x-coordinate for the shrinking of the east-west distances
+        double new_x = x / Math.cos(location.getLatitude());
+
+        result.setLongitude(new_x + location.getLongitude());
+        result.setLatitude(y + location.getLatitude());
+        return result;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mapController.animateTo(currentLocation);
+        addMonsters(currentLocation);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        currentLocation = new GeoPoint(location);
+        mapController.animateTo(currentLocation);
+        mapController.setCenter(currentLocation);
+        if (monsterLocation != null && mLocationOverlay.getMyLocation().distanceTo(monsterLocation) > 150){
+            if (mOverlay.isEnabled())  mOverlay.removeAllItems();
+            addMonsters(currentLocation);
+        } else if (monsterLocation == null){
+            addMonsters(currentLocation);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
